ec7fbb18c0ed8b4aac882688977f0e98044e6d49
progbe <dmdrb94@naver.com>
2019-08-05 20:57:18 +0900

    python

---
+++ Python/variable.py
@@ -0,0 +1,23 @@
+#Udemy lecture
+
+name = "progbe"
+num = 1
+is_ok = True
+#num = name
+
+
+print(num, type(num))
+print(name, type(name))
+print(is_ok, type(is_ok))
+
+#print(num, type(num))
+
+name = "1"
+new_num = int(name)
+print(new_num)
+
+
+
+
+
+
