055d642de82f07e2ddacc633835bc734d235e1b1
progbe <dmdrb94@naver.com>
2019-08-07 21:12:09 +0900

    Udemy lecture

---
+++ Python/list_control.py
@@ -0,0 +1,19 @@
+s = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(s[0])
+
+s[0] = 'X'
+print(s[0])
+
+s[2:5] = []
+print(s)
+
+s.append('F')
+print(s)
+
+s.pop()
+def 
+
+s.remove('b')
+
+s.index(3, 3)
+s.count(3)
